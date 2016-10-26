bfef5a741af41c37ab11c638cd9c3fa6cbf73749
Alex Price <hi@alexpriceonline.com>
2016-10-26 13:21:17 +0100

    Updated google format user

--- dist/hello.all.js
+++ dist/hello.all.js
@@ -4179,10 +4179,9 @@ if (typeof chrome === 'object' && typeof chrome.identity === 'object' && chrome.
 	}
 
 	function formatPerson(o) {
-		o.fullName = o.displayName || o.name;
-		o.pictureUrl = o.picture || (o.image ? o.image.url : null);
-		o.thumbnail = o.picture;
-		console.log(o);
+		o.formattedName = o.displayName || o.name;
+		// + '0' makes the image actually a decent size
+		o.pictureUrl = o.picture + '0' || (o.image ? o.image.url + '0' : null);
 	}
 
 	function formatFriends(o, headers, req) {
